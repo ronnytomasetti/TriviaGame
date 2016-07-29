c4523194efb88a07e685c85368f276338a158875
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-07-29 02:25:11 -0400

    Implemented generation of random questions array

--- assets/js/TriviaGame.js
+++ assets/js/TriviaGame.js
@@ -6,77 +6,78 @@
 
 function TriviaGame()
 {
-  this.questionsBank = [];
-  this.currentQuestion = 0;
-  this.correctAnswers = 0;
-  this.incorrectAnswers = 0;
-  this.unanswered = 0;
-  this.timeAllowed = 10;
-  this.timeRemaining = 0;
-
-  this.getCorrectAnswers = function()
-  {
-    return this.correctAnswers;
-  }
-
-  this.getIncorrectAnswers = function()
-  {
-    return this.incorrectAnswers;
-  }
-
-  this.getUnanswered = function()
-  {
-    return this.unanswered;
-  }
-
-  this.setTimeAllowed = function(value)
-  {
-    this.timeAllowed = value;
-  }
-
-  /**
-  *  Retrieve json object containing trivia questions
-  *  Save data retrieved in trivia.questionsBank[] array.
-  *
-  *  @param ()
-  *  @return undefined
-  *
-  */
-  this.retrieveQuestions = function()
-  {
-    var self = this;
-    $.getJSON( 'assets/js/questions.json', function( data )
-    { 
-      self.questionsBank = $( data.questions );
-    });
-  }
-
-  /**
-  *  Reset time remaining variable to match time allowed
-  *
-  *  @param ()
-  *  @return undefined
-  *
-  */
-  this.resetTimeRemaining = function()
-  {
-    this.timeRemaining = this.timeAllowed;
-  }
-
-  /**
-  *  Reset trivia game counter variables for new round
-  *
-  *  @param ()
-  *  @return undefined
-  *
-  */
-  this.newRound = function()
-  {
+    this.questionsBank = [];
+    this.currentQuestionsArray = [];
     this.currentQuestion = 0;
     this.correctAnswers = 0;
     this.incorrectAnswers = 0;
     this.unanswered = 0;
-    this.resetTimeRemaining();
-  }
+    this.timeAllowed = 10;
+    this.timeRemaining = 0;
+
+    /**
+     *  TODO: IMPLEMENT THIS SOMEWHERE IN MY LOGIC IF POSSIBLE.
+     *  Sets the time allowed variable for question timer.
+     *
+     */
+    this.setTimeAllowed = function( value )
+    {
+        this.timeAllowed = value;
+    };
+
+    /**
+    *  Retrieve json object containing trivia questions
+    *  Save data retrieved in trivia.questionsBank[] array.
+    *
+    */
+    this.retrieveQuestions = function()
+    {
+        var self = this;
+
+        $.getJSON( 'assets/js/questions.json', function( data )
+        {
+            self.questionsBank = data.questions;
+        });
+    };
+
+    /**
+    *  Reset time remaining variable to match time allowed
+    *
+    */
+    this.resetTimeRemaining = function()
+    {
+        this.timeRemaining = this.timeAllowed;
+    };
+
+    /**
+    *  Reset trivia game counter variables for new round
+    *
+    */
+    this.newRound = function()
+    {
+        this.currentQuestion = 0;
+        this.correctAnswers = 0;
+        this.incorrectAnswers = 0;
+        this.unanswered = 0;
+        this.resetTimeRemaining();
+    };
+
+    this.randomizeNewQuestions = function( numberOfQuestions )
+    {
+        // Randomize order of questions array received from json file.
+        var randomizedArray = this.questionsBank.sort( function( a, b ) { return 0.5 - Math.random() } );
+
+        // Initialize new array to store number of questions requested.
+        var generatedArray = [];
+
+        // Run push/pop loop for the total required duration.
+        for ( var i = 0; i < numberOfQuestions; i++ )
+        {
+            generatedArray.push( randomizedArray.pop() );
+        }
+
+        // Store the newly generated array.
+        this.currentQuestionsArray = generatedArray;
+    }
   
 }
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,290 +10,266 @@ var game = {};
 var qTemplate;
 /* Trivia answers template stored in index.html */
 var aTemplate;
+/* Trivia results template stored in index.html */
+var rTemplate;
 /* Declare global time interval variable */
 var timer;
+
+// TODO: IMPLEMENT THE ABILITY FOR THE PLAYER TO CHOOSE THE NUMBER OF QUESTIONS THAT THEY WANT FOR THR TRIVIA GAME
+var numberOfQuestions = 10;
 
 $( document ).ready( function()
 {
 
-  /**
-   *  Starts a new game
-   *
-   *  Initializes global variables and templates.
-   *  Calls function to retrieve json containing questions.
-   *  Binds one click event listener to start button.
-   *
-   *  @param (button) start-button object clicked
-   *  @return undefined
-   */
-   function startGame()
-   {
+    /**
+    *  Starts a new game
+    *
+    *  Initializes global variables and templates.
+    *  Calls function to retrieve json containing questions.
+    *  Binds one click event listener to start button.
+    *
+    *  @param (button) start-button object clicked
+    *  @return undefined
+    */
+    function startGame()
+    {
+        /* Initialize global game variables */
+        game = new TriviaGame();
 
-    /* Initialize global game variables */
-    game = new TriviaGame();
+        /* Start retrieving questions json */
+        game.retrieveQuestions();
 
-    /* Start retrieving questions json */
-    game.retrieveQuestions();
+        qTemplate = $( '#question-template' ).html();
+        aTemplate = $( '#answers-template' ).html();
+        rTemplate = $( '#results-template' ).html();
 
-    qTemplate = $( '#question-template' ).html();
-    aTemplate = $( '#answers-template' ).html();
-    rTemplate = $( '#results-template' ).html();
+        /* Bind event listener to start button */
+        $( 'p.glow-neon' ).one( 'click', startRound );
+    }
+    /* END startGame() ---------------------------- */
 
-    /* Bind event listener to start button */
-    $( 'p.glow-neon' ).one( 'click', startRound );
+    /**
+    *  Starts a new round
+    *
+    *  Hides start button and removes hero img.
+    *  Calls for new round and gets new random questions.
+    *  Ends with call to displayNextQuestion() function.
+    *
+    *  @param ()
+    *  @return undefined
+    *
+    */
+    function startRound()
+    {
+        /* Hide start button */
+        $( 'p.glow-neon' ).addClass( 'hide' );
+        $( 'img.hero-img' ).remove();
 
-   }
-   /* END startGame() ---------------------------- */
+        /* Initiate new round */
+        game.newRound();
+        /* Initiate new set of questions */
+        game.randomizeNewQuestions( numberOfQuestions );
 
-  /**
-   *  Starts a new round
-   *
-   *  Hides start button and removes img.
-   *  Call for new round.
-   *  Ends with call to displayNextQuestion() function.
-   *
-   *  @param ()
-   *  @return undefined
-   *
-   */
-   function startRound()
-   {
-
-    /* Hide start button */
-    $( 'p.glow-neon' ).addClass( 'hide' );
-    $( 'img.hero-img' ).remove();
-
-    /* Initiate new round */
-    game.newRound();
-
-    displayQuestion();
-
-   }
+        displayQuestion();
+    }
    /* END startRound() ---------------------------- */
 
-  /**
-   *  Displays the next question
-   *
-   *  Clone template in order to create question.
-   *  Adds current question and choices to new question template.
-   *  Appends new question to trivia-cards div.
-   *  Starts timer.
-   *
-   *  @param ()
-   *  @return undefined
-   *
-   */
-   function displayQuestion()
-   {
-
-    /* Store copy of question template */
-    var questionTemplate = $( qTemplate ).clone();
-
-    /* Grab question string and choices array from trivia game object */
-    var question = game.questionsBank[ game.currentQuestion ].question;
-    var choices = game.questionsBank[ game.currentQuestion ].choices;
-
-    /* Set up html for current question */
-    $( questionTemplate ).find( '#question' )
-                         .text( question );
-
-    /* Loop through choices array in order to set up html and click listeners */
-    $.each( choices, function( index, value )
+    /**
+    *  Displays the next question
+    *
+    *  Clone template in order to create question.
+    *  Adds current question and choices to new question template.
+    *  Appends new question to trivia-cards div.
+    *  Starts timer.
+    *
+    *  @param ()
+    *  @return undefined
+    *
+    */
+    function displayQuestion()
     {
+        /* Store copy of question template */
+        var questionTemplate = $( qTemplate ).clone();
 
-      $( questionTemplate ).find( '#' + index )
-                           .text( value )
-                           .click( checkAnswer );
+        /* Grab question string and choices array from trivia game object */
+        var question = game.currentQuestionsArray[ game.currentQuestion ].question;
+        var choices = game.currentQuestionsArray[ game.currentQuestion ].choices;
 
-    }); /* END .each() loop */
+        /* Set up html for current question */
+        $( questionTemplate ).find( '#question' )
+                             .text( question );
 
-    /* Add question to div with id trivia-cards */
-    $( '#trivia-cards' ).children().remove();
-    $( '#trivia-cards' ).append( questionTemplate );
+        /* Loop through choices array in order to set up html and click listeners */
+        $.each( choices, function( index, value )
+        {
+            $( questionTemplate ).find( '#' + index )
+                                 .text( value )
+                                 .click( checkAnswer );
+        });
 
-    /* Time starts now!! */
-    startTimer();
+        /* Add question to div with id trivia-cards */
+        $( '#trivia-cards' ).children().remove();
+        $( '#trivia-cards' ).append( questionTemplate );
 
-   }
-   /* END displayQuestion() ---------------------------- */
-
-  /**
-   *  Checks answer after every question
-   *
-   *  Clear previous questions timer.
-   *  Unbinds click event from choices.
-   *  Compare user's answer with correct answer.
-   *
-   *  @param (object) element clicked
-   *  @return undefined
-   *
-   */
-   function checkAnswer()
-   {
-
-    clearTimer();
-
-    /* NOTE: THIS IS PROBABLY USELESS SINCE LATER ON IN THE CODE 
-              I'M REMOVING ALL CHILDREN FROM TRIVIA CARD DIV!!! */
-    $( '#trivia-cards' ).children()
-                        .unbind( 'click', checkAnswer );
-
-    var userChoice = $( this ).attr( 'id' );
-    var correctAnswer = game.questionsBank[ game.currentQuestion ].answer;
-
-    if ( userChoice == correctAnswer ) /* Player answered correctly */
-    {
-      game.correctAnswers++;
-      displayAnswer( true );
+        /* Time starts now!! */
+        startTimer();
     }
-    else if ( userChoice != correctAnswer && userChoice <= 3 ) /* Player answered in time but was incorrect */
+    /* END displayQuestion() ---------------------------- */
+
+    /**
+    *  Checks answer after every question
+    *
+    *  Clear previous questions timer.
+    *  Unbinds click event from choices.
+    *  Compare user's answer with correct answer.
+    *
+    *  @param (object) element clicked
+    *  @return undefined
+    *
+    */
+    function checkAnswer()
     {
-      game.incorrectAnswers++;
-      displayAnswer( false );
+        clearTimer();
+
+        /* NOTE: THIS IS PROBABLY USELESS SINCE LATER ON IN THE CODE
+                  I'M REMOVING ALL CHILDREN FROM TRIVIA CARD DIV!!! */
+        $( '#trivia-cards' ).children()
+                            .unbind( 'click', checkAnswer );
+
+        var userChoice = $( this ).attr( 'id' );
+        var correctAnswer = game.currentQuestionsArray[ game.currentQuestion ].answer;
+
+        if ( userChoice == correctAnswer ) /* Player answered correctly */
+        {
+            game.correctAnswers++;
+            displayAnswer( true );
+        }
+        else if ( userChoice != correctAnswer && userChoice <= 3 ) /* Player answered in time but was incorrect */
+        {
+            game.incorrectAnswers++;
+            displayAnswer( false );
+        }
+        else /* Player did not answer question in time */
+        {
+            game.unanswered++;
+            displayAnswer( false );
+        }
     }
-    else /* Player did not answer question in time */
+    /* END checkAnswer() ---------------------------- */
+
+    /**
+    *  Displays the answer to previous question
+    *
+    *  If more questions are available, shows next question.
+    *  Else, show final results of trivia round.
+    *
+    *  @param (bool) true, if user answered question correctly
+    *  @return undefined
+    *
+    */
+    function displayAnswer( isCorrect )
     {
-      game.unanswered++;
-      displayAnswer( false );
+        /* Store copy of answer template */
+        var answerTemplate = $( aTemplate ).clone();
+
+        var answer = game.currentQuestionsArray[ game.currentQuestion ].answer;
+        var answerString = game.currentQuestionsArray[ game.currentQuestion ].choices[ answer ];
+
+        $( answerTemplate ).find( '#result' )
+                           .text( ( isCorrect ? "CORRECT!" : "WRONG!" ) );
+
+        $( answerTemplate ).find( '#correct-answer' )
+                           .text( answerString );
+
+        $( '#trivia-cards' ).children().remove();
+        $( '#trivia-cards' ).append( answerTemplate );
+
+        game.currentQuestion++;
+
+        if ( game.currentQuestion == game.questionsBank.length )
+          setTimeout( endRound, 3000 ); // NO MORE QUESTIONS!! END ROUND BEFORE IT BREAKS!!
+        else
+          setTimeout( displayQuestion, 3000 ); // YEAH!!! MORE QUESTIONS, DISPLAY NEXT!
+    }
+    /* END displayAnswer() ---------------------------- */
+
+    /**
+    *  Ends the current trivia round
+    *
+    *  Print results of previous trivia round
+    *  Displays new round button to restart
+    *
+    *  @param ()
+    *  @return undefined
+    *
+    */
+    function endRound()
+    {
+        var resultsTemplate = $( rTemplate ).clone();
+
+        $( resultsTemplate ).find( '#correct' )
+                            .text( 'Correct: ' + game.correctAnswers );
+        $( resultsTemplate ).find( '#incorrect' )
+                            .text( 'Incorrect: ' + game.incorrectAnswers );
+        $( resultsTemplate ).find( '#unanswered' )
+                            .text( 'Unanswered: ' + game.unanswered );
+
+        $( '#trivia-cards' ).children().remove();
+        $( '#trivia-cards' ).append( resultsTemplate );
+
+        $( 'p.glow-neon' ).text( 'Restart Trivia' )
+                          .removeClass( 'hide' )
+                          .one( 'click', startRound );
+    }
+    /* END endRound() ---------------------------- */
+
+    /**
+    *  Creates new timer object
+    *
+    *  @param ()
+    *  @return undefined
+    *
+    */
+    function startTimer()
+    {
+        $( '.circle-timer' ).TimeCircles( { time: {
+                                                    Days: { show: false },
+                                                    Hours: { show: false },
+                                                    Minutes: { show: false },
+                                                    Seconds: { color: "#ff8080" }
+                                                    },
+                                                    direction: "Both",
+                                                    count_past_zero: false,
+                                                    total_duration: game.timeAllowed
+                                            });
+
+        $( '.circle-timer' ).attr( 'data-timer', game.timeAllowed.toString() );
+        $( '.circle-timer' ).TimeCircles().start();
+
+        /* Set new timing interval */
+        timer = setInterval( updateTimeRemaining, 1000 );
+    }
+    /* END startTimer() ---------------------------- */
+
+    function updateTimeRemaining()
+    {
+        game.timeRemaining--;
+
+        if ( game.timeRemaining == 0 )
+            checkAnswer();
     }
 
-   }
-   /* END checkAnswer() ---------------------------- */
+    function clearTimer()
+    {
+       $( '.circle-timer' ).TimeCircles().destroy();
 
-  /**
-   *  Displays the answer to previous question
-   *
-   *  If more questions are available, shows next question.
-   *  Else, show final results of trivia round.
-   *
-   *  @param (bool) true, if user answered question correctly
-   *  @return undefined
-   *
-   */
-   function displayAnswer( isCorrect )
-   {
+       clearInterval( timer );
 
-    /* Store copy of answer template */
-    var answerTemplate = $( aTemplate ).clone();
+       game.resetTimeRemaining();
+    }
 
-    var answer = game.questionsBank[ game.currentQuestion ].answer;
-    var answerString = game.questionsBank[ game.currentQuestion ].choices[ answer ];
-
-    $( answerTemplate ).find( '#result' )
-                       .text( ( isCorrect ? "CORRECT!" : "WRONG!" ) );
-
-    $( answerTemplate ).find( '#correct-answer' )
-                       .text( answerString );
-
-    $( '#trivia-cards' ).children().remove();
-    $( '#trivia-cards' ).append( answerTemplate );
-
-    game.currentQuestion++;
-
-    if ( game.currentQuestion == game.questionsBank.length )
-      setTimeout( endRound, 3000 ); // NO MORE QUESTIONS! END ROUND!!
-    else
-      setTimeout( displayQuestion, 3000 ); // YEAH!!! MORE QUESTIONS, DISPLAY NEXT!
-
-   }
-   /* END displayAnswer() ---------------------------- */
-
-  /**
-   *  Ends the current trivia round
-   *
-   *  Print results of previous trivia round
-   *  Displays new round button to restart
-   *
-   *  @param ()
-   *  @return undefined
-   *
-   */
-   function endRound()
-   {
-
-    var resultsTemplate = $( rTemplate ).clone();
-
-    $( resultsTemplate ).find( '#correct' )
-                        .text( 'Correct: ' + game.correctAnswers );
-    $( resultsTemplate ).find( '#incorrect' )
-                        .text( 'Incorrect: ' + game.incorrectAnswers );
-    $( resultsTemplate ).find( '#unanswered' )
-                        .text( 'Unanswered: ' + game.unanswered );
-
-    $( '#trivia-cards' ).children().remove();
-    $( '#trivia-cards' ).append( resultsTemplate );
-
-    $( 'p.glow-neon' ).text( 'Restart Trivia' )
-                              .removeClass( 'hide' )
-                              .one( 'click', startRound );
-
-   }
-   /* END endRound() ---------------------------- */
-
-  /**
-   *  Creates new timer object
-   *
-   *  @param ()
-   *  @return undefined
-   *
-   */
-   function startTimer()
-   {
-
-    $( '.circle-timer' ).TimeCircles({ time: { Days: { show: false },
-                                               Hours: { show: false },
-                                               Minutes: { show: false },
-                                               Seconds: { color: "#ff8080" }
-                                             },
-                                             total_duration: game.timeAllowed
-                                           });
-
-    $( '.circle-timer' ).TimeCircles( { direction: "Both" } );
-    // $( '.circle-timer' ).TimeCircles( { use_background: false } );
-    // $( '.circle-timer' ).TimeCircles( { count_past_zero: false } );
-
-    $( '.circle-timer' ).attr( 'data-timer', game.timeAllowed.toString() );
-    $( '.circle-timer' ).TimeCircles().start();
-
-    /* Set new timing interval */
-    timer = setInterval( updateTimeRemaining, 1000 );
-
-   }
-   /* END startTimer() ---------------------------- */
-
-  /**
-   *  Updates time remaining variable.
-   *  Checks if time allowed equals zero, if true fires checkAnswer().
-   *
-   *  @param ()
-   *  @return undefined
-   *
-   */
-   function updateTimeRemaining()
-   {
-
-    game.timeRemaining--;
-
-    if ( game.timeRemaining == 0 )
-      checkAnswer();
-
-   }
-   /* END updateTimer() ---------------------------- */
-
-   function clearTimer()
-   {
-
-    $( '.circle-timer' ).TimeCircles().destroy();
-
-    clearInterval( timer );
-
-    game.resetTimeRemaining();
-
-   }
-
-  /** --------------------------
-   *  APPLICATION ENTRY POINT *
-   ------------------------- **/
-   startGame();
-
+    /** --------------------------
+    *  APPLICATION ENTRY POINT *
+    ------------------------- **/
+    startGame();
 });
